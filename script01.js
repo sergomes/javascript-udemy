4a07490c50a0e38cba73e2d61bf557852a340302
Sérgio Gomes <sergiotecdev@gmail.com>
2023-11-17 14:32:09 -0300

    variáveis

---
+++ script01.js
@@ -0,0 +1,20 @@
+
+/*
+
+SISTEMA DE CADASTRO DO BANCO
+
+*/
+
+let nome = "Manuel";
+let sobreNome = "Silva";
+let idade = 24;
+const numcliente = 095864;
+
+idade = 25;
+
+
+console.log(nome);
+console.log(sobreNome);
+console.log(idade);
+console.log(numcliente);
+
