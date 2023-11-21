16dcd5bba7045f30a01b8a33d071aedf6d89667a
Sérgio Gomes <sergiotecdev@gmail.com>
2023-11-21 14:08:35 -0300

    Realizando comparações com JavaScript

--- script01.js
+++ script01.js
@@ -6,15 +6,27 @@ SISTEMA DE CADASTRO DO BANCO
 */
 
 let nome = "Manuel";
-let sobreNome = "Silva";
+let sobrenome = "Silva";
 let idade = 24;
 const numcliente = 095864;
+let valorEmprestimo = 150000;
+let texaDeJuros = 0.10;
+let numAnos = 6;
+let ehBomPagador = true;
 
-idade = 25;
+/*
+
+  Montante = valorEmprestimo + juros
+  juros = valorEmprestimo * taxaDeJuros * numAnos
+
+*/
+
+let juros = valorEmprestimo * texaDeJuros * numAnos;
+let montante = valorEmprestimo + juros;
+
+console.log("Valor do montante: " + montante);
 
 
-console.log(nome);
-console.log(sobreNome);
-console.log(idade);
-console.log(numcliente);
+
+
 
---
+++ script02.js
@@ -0,0 +1,12 @@
+
+/*
+
+DESAFIO 01
+
+*/
+
+let nomeProduto = "Toddy";
+let preco = 6.99;
+let validade = true;
+
+
