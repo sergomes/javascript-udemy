7810d58932dd599bcf2ac262add8b4c3f9de5e37
Sérgio Gomes <sergiotecdev@gmail.com>
2023-11-22 07:59:04 -0300

    seção 3

--- script02.js
+++ script02.js
@@ -5,8 +5,22 @@ DESAFIO 01
 
 */
 
-let nomeProduto = "Toddy";
-let preco = 6.99;
-let validade = true;
+// let nomeProduto = "Toddy";
+// let preco = 6.99;
+// let validade = true;
 
 
+/*
+
+DESAFIO 02
+
+*/
+
+let bimestre01 = 9;
+let bimestre02 = 7;
+let bimestre03 = 8;
+let bimestre04 = 6;
+
+let media = (bimestre01 + bimestre02 + bimestre03 + bimestre04) / 4;
+
+console.log(media);
